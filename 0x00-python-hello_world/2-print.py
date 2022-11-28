4644857a5d6a484f20d03977fcc1f1ccad2f21b0
Saibantoril <saheedolabanji@gmail.com>
2022-11-28 06:17:23 -0800

    updated first task

--- 0x00-python-hello_world/2-print.py
+++ 0x00-python-hello_world/2-print.py
@@ -1,2 +1,2 @@
 #!/bin/bash
-print ("Programming is like building a multilingual puzzle, followed by a new line.")
+print ("Programming is like building a multilingual puzzle, followed by a new line.\n")
