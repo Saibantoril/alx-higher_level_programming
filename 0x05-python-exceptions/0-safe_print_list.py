ce2d0d33f1bdbdf2d17905d8d6acb7920a59541a
Saibantoril <saheedolabanji@gmail.com>
2022-12-19 07:14:13 -0800

    task 1

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -4,7 +4,7 @@ def safe_print_list(my_list=[], x=0):
     counted_list = 0
     for i in range (x):
         try:
-            print(f"{my_list[i]}", end= " ")
+            print(f"{my_list[i]}", end= "")
             counted_list += 1
         except IndexError:
             break
