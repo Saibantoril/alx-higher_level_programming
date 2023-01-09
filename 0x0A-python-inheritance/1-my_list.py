cecb543022f89aac0117b000564d8a4d1923abe8
Saibantoril <saheedolabanji@gmail.com>
2023-01-09 08:00:38 -0800

    task 0 to 7

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 """This module inherits from the list class"""
 
-class Mylist(list):
+
+class MyList(list):
     """A class that inherits from list"""
     def print_sorted(self):
-        """prints sorted list"""
+        """prints a sorted list"""
         print(sorted(self))
