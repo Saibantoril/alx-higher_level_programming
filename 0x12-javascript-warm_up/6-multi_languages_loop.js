2954b4c75e5e2d02ffc60a1a10b882155b8947bd
Saibantoril <saheedolabanji@gmail.com>
2023-03-13 11:32:00 -0700

    task 7

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,7 @@
 #!/usr/bin/node
-const myVar = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 
-myVar.forEach((element) => {
-  console.log(element);
-});
+const lines = ["C is fun", "Python is cool", "JavaScript is amazing"];
+
+for (let i = 0; i < lines.length; i++) {
+  console.log(lines[i]);
+}
