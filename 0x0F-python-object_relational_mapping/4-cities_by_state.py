436b10549f42b027e6d93911cad5a36e11fe7da4
Saibantoril <saheedolabanji@gmail.com>
2023-03-18 07:54:18 -0700

    task 4

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""
+Script that lists all cities from the database hbtn_0e_4_usa
+"""
+
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+    # Command line arguments
+    mysql_username = sys.argv[1]
+    mysql_password = sys.argv[2]
+    db_name = sys.argv[3]
+
+    # Connect to MySQL database
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=mysql_username,
+        passwd=mysql_password,
+        db=db_name
+    )
+
+    # Create cursor to execute queries
+    cur = db.cursor()
+
+    # Execute query
+    query = "SELECT cities.id, cities.name, states.name FROM cities JOIN states ON cities.state_id = states.id ORDER BY cities.id ASC"
+    cur.execute(query)
+
+    # Fetch all rows and print results
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+
+    # Close cursor and database connection
+    cur.close()
+    db.close()
