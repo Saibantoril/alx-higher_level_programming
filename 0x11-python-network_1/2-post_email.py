6711bb1ddf4beafc926e4b4259e95615788daca5
Saibantoril <saheedolabanji@gmail.com>
2023-03-31 06:30:35 -0700

    task 2

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,16 +1,18 @@
 #!/usr/bin/python3
-'''A python script that sends post request to the passed URL
-and displays the body of the response'''
-
-import urllib.request
-import urllib.parse
+"""Sends a POST request to a given URL with a given email.
+Usage: ./2-post_email.py <URL> <email>
+  - Displays the body of the response.
+"""
 import sys
+import urllib.parse
+import urllib.request
+
 
 if __name__ == "__main__":
     url = sys.argv[1]
-    email = sys.argv[2]
-    data = urllib.parse.urlencode({'email': email})
-    data = data.encode('utf-8')
-    with urllib.request.urlopen(url, data) as response:
-        html = response.read().decode('utf-8')
-        print(html)
+    value = {"email": sys.argv[2]}
+    data = urllib.parse.urlencode(value).encode("ascii")
+
+    request = urllib.request.Request(url, data)
+    with urllib.request.urlopen(request) as response:
+        print(response.read().decode("utf-8"))
