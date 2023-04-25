13bb7d83ccae2230471241d86f2f6bac09b8a8a0
root <saheedolabanji@gmail.com>
2023-04-25 08:37:12 -0700

    updated advanced task

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,26 +1,30 @@
 #!/usr/bin/node
 
 const request = require('request');
-
 const movieId = process.argv[2];
-const url = `https://swapi.dev/api/films/${movieId}/`;
+const url = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
 
-request.get(url, (error, response, body) => {
+function getCharacterName(characterUrl) {
+  return new Promise((resolve, reject) => {
+    request(characterUrl, function (error, response, body) {
+      if (error) {
+        reject(error);
+      } else {
+        const characterData = JSON.parse(body);
+        resolve(characterData.name);
+      }
+    });
+  });
+}
+
+request(url, async function (error, response, body) {
   if (error) {
     console.log(error);
-    return;
-  }
-
-  const data = JSON.parse(body);
-  const characters = data.characters;
-  for (const character of characters) {
-    request(character, (error, response, body) => {
-      if (error) {
-        console.log(error);
-        return;
-      }
-      const characterData = JSON.parse(body);
-      console.log(characterData.name);
-    });
+  } else {
+    const data = JSON.parse(body);
+    for (const characterUrl of data.characters) {
+      const name = await getCharacterName(characterUrl);
+      console.log(name);
+    }
   }
 });
